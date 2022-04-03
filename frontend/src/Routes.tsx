3c5747cb73f404a256ae89912dbb91d9dcc53bdd
yukiyamamuro <yuki.sumaho@gmail.com>
2022-04-03 20:13:09 +0900

    chore: save

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { UserListPage } from "./components/UserPage/UserListPage";
+import { WorkflowListPage } from "./components/WorkflowPage/WorkflowListPage";
 
 export const RoutesManager: FC = () => {
   return(
     <BrowserRouter>
       <Routes>
-        <Route path="/user" element={<UserListPage />}/>
+        <Route path="/users" element={<UserListPage />}/>
+        <Route path="/workflow" element={<WorkflowListPage />} />
       </Routes>
     </BrowserRouter>
   )
